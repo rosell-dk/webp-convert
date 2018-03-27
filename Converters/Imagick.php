c64bacfd205dafd4064191ceaf7368404055bb63
S1SYPHOS <hello@twobrain.io>
2018-03-27 13:40:33 +0200

    Refactoring Imagick.php

--- Converters/Imagick.php
+++ Converters/Imagick.php
@@ -2,69 +2,88 @@
 
 namespace WebPConvert\Converters;
 
-function webpconvert_imagick($source, $destination, $quality, $strip_metadata)
+class Imagick
 {
-    if (!extension_loaded('imagick')) {
-        return 'This implementation requires the imagick extension, which you do not have';
+    // Throws an exception if iMagick does not support WebP conversion
+    protected static function hasWebpSupport($object)
+    {
+        if (!in_array('WEBP', $object->queryFormats())) {
+            throw new \Exception('iMagick was compiled without WebP support.');
+        }
+        $object->setImageFormat('WEBP');
     }
 
-    try {
-        $im = new Imagick($source);
-    } catch (Exception $e) {
-        return 'imagick is installed but cannot handle source file. Error message: ' . $e->getMessage();
-    }
+    // Throws an exception if the provided file's extension is unsupported
+    protected static function isValidExtension($filePath, $object)
+    {
+        $fileExtension = pathinfo($filePath, PATHINFO_EXTENSION);
+        $fileExtension = strtolower($fileExtension);
 
-    try {
-        $im->setImageFormat("WEBP");
-    } catch (Exception $e) {
-        return 'imagick is installed, but not compiled with webp support';
-        // return $e->getMessage();
-        // echo 'Available formats:' . implode(', ', $im->queryFormats());
-    }
-
-    // About webp options:
-    // http://www.imagemagick.org/script/webp.php
-    // https://stackoverflow.com/questions/37711492/imagemagick-specific-webp-calls-in-php
-
-
-    if (defined("WEBPCONVERT_IMAGICK_METHOD")) {
-        $im->setOption('webp:method', WEBPCONVERT_IMAGICK_METHOD);
-    } else {
-        $im->setOption('webp:method', '6');
-    }
-
-    if (defined("WEBPCONVERT_IMAGICK_LOW_MEMORY")) {
-        $im->setOption('webp:low-memory', (
-            WEBPCONVERT_IMAGICK_LOW_MEMORY
-            ? 'true'
-            : 'false'
-        ));
-    } else {
-        $im->setOption('webp:low-memory', 'true');
-    }
-
-    $im->setImageCompressionQuality($quality);
-
-    $parts = explode('.', $source);
-    $ext = array_pop($parts);
-    switch (strtolower($ext)) {
-        case 'jpg':
-        case 'jpeg':
-            break;
-        case 'png':
-            $im->setOption('webp:lossless', 'true');
-            break;
-        default:
-            return 'Unsupported file extension';
-    }
-
-    $success = $im->writeImage($destination);
-    // This would also works:
-    // $success = file_put_contents($destination, $im);
-
-    if ($success) {
+        switch ($fileExtension) {
+            case 'jpg':
+            case 'jpeg':
+                break;
+            case 'png':
+                $object->setOption('webp:lossless', 'true');
+                break;
+            default:
+                throw new \Exception('Unsupported file extension: ' . $fileExtension);
+        }
         return true;
-    } else {
-        return 'writeImage("' . $destination . '") failed';
+    }
+
+    public static function convert($source, $destination, $quality, $stripMetadata)
+    {
+        try {
+            if (!extension_loaded('imagick')) {
+                throw new \Exception('Required iMagick extension is not available.');
+            }
+
+            if (!class_exists('Imagick')) {
+                throw new \Exception('iMagick is installed but cannot handle source file.');
+            }
+
+            $im = new \Imagick($source);
+
+            self::hasWebpSupport($im);
+            self::isValidExtension($source, $im);
+        } catch (\Exception $e) {
+            return false; // TODO: `throw` custom \Exception $e & handle it smoothly on top-level.
+        }
+
+        /*
+         * More about iMagick's WebP options:
+         * http://www.imagemagick.org/script/webp.php
+         * https://stackoverflow.com/questions/37711492/imagemagick-specific-webp-calls-in-php
+         */
+
+        if (defined('WEBPCONVERT_IMAGICK_METHOD')) {
+            $im->setOption('webp:method', WEBPCONVERT_IMAGICK_METHOD);
+        } else {
+            $im->setOption('webp:method', '6');
+        }
+
+        if (defined('WEBPCONVERT_IMAGICK_LOW_MEMORY')) {
+            $im->setOption('webp:low-memory', (
+                WEBPCONVERT_IMAGICK_LOW_MEMORY
+                ? 'true'
+                : 'false'
+            ));
+        } else {
+            $im->setOption('webp:low-memory', 'true');
+        }
+
+        $im->setImageCompressionQuality($quality);
+
+        // TODO: Check out other iMagick methods, see http://php.net/manual/de/imagick.writeimage.php#114714
+        // 1. file_put_contents($destination, $im)
+        // 2. $im->writeImage($destination)
+        $success = $im->writeImageFile(fopen($destination, 'wb'));
+
+        if ($success) {
+            return true;
+        } else {
+            return 'writeImage("' . $destination . '") failed';
+        }
     }
 }
