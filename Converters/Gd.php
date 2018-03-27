7902e2a3676cfed4f0c74b995bdad1343bab200a
S1SYPHOS <hello@twobrain.io>
2018-03-27 13:38:56 +0200

    Refactoring Gd.php

--- Converters/Gd.php
+++ Converters/Gd.php
@@ -2,60 +2,66 @@
 
 namespace WebPConvert\Converters;
 
-function webpconvert_gd($source, $destination, $quality, $strip_metadata)
+class Gd
 {
-    if (!extension_loaded('gd')) {
-        return 'This implementation requires the GD extension, which you do not have';
-    }
-
-    if (!function_exists('imagewebp')) {
-        return 'imagewebp() is not available';
-    }
-
-    $parts = explode('.', $source);
-    $ext = array_pop($parts);
-    $image = '';
-
-    switch (strtolower($ext)) {
-        case 'jpg':
-        case 'jpeg':
-            $image = imagecreatefromjpeg($source);
-            break;
-        case 'png':
-            if (defined("WEBPCONVERT_GD_PNG") && WEBPCONVERT_GD_PNG) {
-                $image = imagecreatefrompng($source);
-            } else {
-                return 'This converter has poor handling of PNG images and therefore refuses to convert the image. You can however force it to convert PNGs as well like this: define("WEBPCONVERT_GD_PNG", true);';
+    public static function convert($source, $destination, $quality, $stripMetadata)
+    {
+        try {
+            if (!extension_loaded('gd')) {
+                throw new \Exception('Required GD extension is not available.');
             }
-            break;
-        default:
-            return 'Unsupported file extension';
-    }
 
-    if (!$image) {
-        // Either imagecreatefromjpeg or imagecreatefrompng returned false
-        return 'Either imagecreatefromjpeg or imagecreatefrompng failed';
-    }
+            if (!function_exists('imagewebp')) {
+                throw new \Exception('Required imagewebp() function is not available.');
+            }
+        } catch (\Exception $e) {
+            return false; // TODO: `throw` custom \Exception $e & handle it smoothly on top-level.
+        }
 
-    $success = imagewebp($image, $destination, $quality);
+        $parts = explode('.', $source);
+        $ext = array_pop($parts);
+        $image = '';
 
+        switch (strtolower($ext)) {
+            case 'jpg':
+            case 'jpeg':
+                $image = imagecreatefromjpeg($source);
+                break;
+            case 'png':
+                if (defined("WEBPCONVERT_GD_PNG") && WEBPCONVERT_GD_PNG) {
+                    $image = imagecreatefrompng($source);
+                } else {
+                    return 'This converter has poor handling of PNG images and therefore refuses to convert the image. You can however force it to convert PNGs as well like this: define("WEBPCONVERT_GD_PNG", true);';
+                }
+                break;
+            default:
+                return 'Unsupported file extension';
+        }
 
-    /*
-     * This hack solves an `imagewebp` bug
-     * See https://stackoverflow.com/questions/30078090/imagewebp-php-creates-corrupted-webp-files
-     *
-     */
+        if (!$image) {
+            // Either imagecreatefromjpeg or imagecreatefrompng returned false
+            return 'Either imagecreatefromjpeg or imagecreatefrompng failed';
+        }
 
-    if (filesize($destination) % 2 == 1) {
-        file_put_contents($destination, "\0", FILE_APPEND);
-    }
+        $success = imagewebp($image, $destination, $quality);
 
-    // Hm... sometimes I get completely transparent images, even with the hack above. Help, anybody?
+        /*
+         * This hack solves an `imagewebp` bug
+         * See https://stackoverflow.com/questions/30078090/imagewebp-php-creates-corrupted-webp-files
+         *
+         */
 
-    imagedestroy($image);
-    if ($success) {
-        return true;
-    } else {
-        return 'imagewebp() call failed';
+        if (filesize($destination) % 2 == 1) {
+            file_put_contents($destination, "\0", FILE_APPEND);
+        }
+
+        // Hm... sometimes I get completely transparent images, even with the hack above. Help, anybody?
+
+        imagedestroy($image);
+        if ($success) {
+            return true;
+        } else {
+            return 'imagewebp() call failed';
+        }
     }
 }
