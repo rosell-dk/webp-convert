299726608e25ada64bb8b348f3adfca15b132e8b
M. Folkers <hello@twobrain.io>
2018-03-28 17:19:41 +0200

    Changing createFolder() to incorporate permissions check & deleteFile()

--- WebPConvert.php
+++ WebPConvert.php
@@ -34,50 +34,56 @@ class WebPConvert
         return true;
     }
 
-    // Returns the provided file's folder name
-    protected static function getFilePath($filePath)
-    {
-        return pathinfo($filePath, PATHINFO_DIRNAME);
-    }
-
     // Creates the provided folder & sets correct permissions
-    public static function createFolder($path)
+    public static function createFolder($filePath)
     {
-        // TODO: what if this is outside open basedir?
-        // see http://php.net/manual/en/ini.core.php#ini.open-basedir
+        $folder = pathinfo($filePath, PATHINFO_DIRNAME);
 
-        // First, we have to figure out which permissions to set.
-        // We want same permissions as parent folder
-        // But which parent? - the parent to the first missing folder
+        if (!file_exists($folder)) {
+            // TODO: what if this is outside open basedir?
+            // see http://php.net/manual/en/ini.core.php#ini.open-basedir
 
-        $parentFolders = explode('/', $path);
-        $poppedFolders = array();
+            // First, we have to figure out which permissions to set.
+            // We want same permissions as parent folder
+            // But which parent? - the parent to the first missing folder
 
-        while (!(file_exists(implode('/', $parentFolders)))) {
-            array_unshift($poppedFolders, array_pop($parentFolders));
+            $parentFolders = explode('/', $folder);
+            $poppedFolders = array();
+
+            while (!(file_exists(implode('/', $parentFolders)))) {
+                array_unshift($poppedFolders, array_pop($parentFolders));
+            }
+
+            // Retrieving permissions of closest existing folder
+            $closestExistingFolder = implode('/', $parentFolders);
+            $permissions = fileperms($closestExistingFolder) & 000777;
+
+            // Trying to create the given folder
+            if (!mkdir($folder, $permissions, true)) {
+                 throw new \Exception('Failed creating folder: ' . $folder);
+            }
+
+            // `mkdir` doesn't respect permissions, so we have to `chmod` each created subfolder
+            foreach ($poppedFolders as $subfolder) {
+                $closestExistingFolder .= '/' . $subfolder;
+                // Setting directory permissions
+                chmod($folder, $permissions);
+            }
         }
 
-        // Retrieving permissions of closest existing folder
-        $closestExistingFolder = implode('/', $parentFolders);
-        $permissions = fileperms($closestExistingFolder) & 000777;
+        // Checks file & folder writing permissions and deletes file in provided $filePath if already present
+        if (file_exists($filePath)) {
+            if (!is_writable($filePath)) {
+                 throw new \Exception('Cannot overwrite ' . basename($filePath) . ' - check file permissions.');
+            }
 
-        // Trying to create the given folder
-        if (!mkdir($path, $permissions, true)) {
-             throw new \Exception('Failed creating folder: ' . $path);
-        }
-
-        // `mkdir` doesn't respect permissions, so we have to `chmod` each created subfolder
-        foreach ($poppedFolders as $subfolder) {
-            $closestExistingFolder .= '/' . $subfolder;
-            // Setting directory permissions
-            chmod($path, $permissions);
-        }
-    }
-
-    protected static function deleteFile($file)
-    {
-        if (!unlink($file)) {
-            throw new \Exception('File already exists and cannot be removed: ' . $file);
+            if (!unlink($filePath)) {
+                throw new \Exception('File already exists and cannot be removed: ' . $file);
+            }
+        } else {
+            if (!is_writable($folder)) {
+                 throw new \Exception('Cannot write ' . basename($filePath) . ' - check folder permissions.');
+            }
         }
         return true;
     }
@@ -123,32 +129,7 @@ class WebPConvert
         try {
             self::isValidTarget($source);
             self::isAllowedExtension($source);
-
-            // Prepares destination folder
-            $destinationFolder = self::getFilePath($destination);
-
-            // Checks if the provided destination folder exists
-            if (!file_exists($destinationFolder)) {
-                // If not, attempt to create it
-                self::createFolder($destinationFolder);
-            }
-
-            // Checks file & folder writing permissions if provided $destination doesn't exist
-            if (file_exists($destination)) {
-                if (!is_writable($destination)) {
-                     throw new \Exception('Cannot overwrite ' . basename($destination) . ' - check file permissions.');
-                }
-            } else {
-                if (!is_writable($destinationFolder)) {
-                     throw new \Exception('Cannot write ' . basename($destination) . ' - check folder permissions.');
-                }
-            }
-
-            // Checks if there's already a converted file at $destination
-            if (file_exists($destination)) {
-                // If so, attempt to remove it
-                self::deleteFile($destination);
-            }
+            self::createFolder($destination);
 
             foreach (self::getConverters() as $converter) {
                 $converter = ucfirst($converter);
