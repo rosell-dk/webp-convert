cc384e241ce03600dab2545588c666e44f8a2df7
S1SYPHOS <hello@twobrain.io>
2018-03-30 11:57:56 +0200

    Improving error handling in Ewww.php

--- Converters/Ewww.php
+++ Converters/Ewww.php
@@ -4,45 +4,56 @@ namespace WebPConvert\Converters;
 
 class Ewww
 {
-    // Checks if all requirements are met, in which case curl_init() is returned
-    protected static function initCurl($curl_file_create = true)
+    // Checks if all requirements of cURL are met
+    protected static function checkRequirements($curl_file_create = true)
     {
         if (!extension_loaded('curl')) {
             throw new \Exception('Required cURL extension is not available.');
         }
 
-        if (!function_exists('url_init')) {
+        if (!function_exists('curl_init')) {
             throw new \Exception('Required url_init() function is not available.');
         }
 
-        $curlInit = curl_init();
-        if (!$curlInit) {
-            throw new \Exception('Could not initialise cURL.');
-        }
-
-        if (!$curl_file_create && function_exists('curl_file_create')) {
+        if (!$curl_file_create && !function_exists('curl_file_create')) {
             throw new \Exception('Required curl_file_create() function is not available (requires PHP > 5.5).');
         }
 
-        if (!defined('WEBPCONVERT_EWWW_KEY')) {
+        if (!defined("WEBPCONVERT_EWWW_KEY")) {
             throw new \Exception('Missing API key.');
         }
-
-        return $curlInit;
+        
+        return true;
     }
 
     // Throws an exception if the provided API key is invalid
-    public static function isValidKey($key)
+    public static function isValidKey($key = WEBPCONVERT_EWWW_KEY)
     {
         try {
-            $curlInit = self::initCurl(false);
+            self::checkRequirements(false);
+
+            $ch = curl_init();
+            if (!$ch) {
+                throw new \Exception('Could not initialise cURL.');
+            }
+
+            $headers = array();
+            $headers[] = "Content-Type: application/x-www-form-urlencoded";
+
+            curl_setopt($ch, CURLOPT_URL, "https://optimize.exactlywww.com/quota/");
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, "api_key=' . $key . '");
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+            $response = curl_exec($ch);
+
+            if (curl_errno($ch)) {
+                throw new \Exception(curl_error($ch));
+            }
         } catch (\Exception $e) {
             echo 'Error: ' . $e->getMessage();
         }
-
-        curl_setopt($curlInit, CURLOPT_URL, 'https://optimize.exactlywww.com/verify/');
-        curl_setopt($curlInit, CURLOPT_POSTFIELDS, array('api_key' => $key));
-        $response = curl_exec($curlInit);
 
         /*
          * There are three possible responses:
@@ -51,8 +62,8 @@ class Ewww
          * '' = an empty response indicates that the key is not valid
         */
 
-        $result[] = 'Verify returned "' . $response . '"';
-        curl_close($curlInit);
+        $result[] = $response;
+        curl_close($ch);
 
         return implode($result, '<br>');
     }
@@ -60,41 +71,49 @@ class Ewww
     public static function convert($source, $destination, $quality, $stripMetadata)
     {
         try {
-            $curlInit = self::initCurl();
+            self::checkRequirements();
+
+            $ch = curl_init();
+            if (!$ch) {
+                throw new \Exception('Could not initialise cURL.');
+            }
+
+            $curlOptions = array(
+                'api_key' => WEBPCONVERT_EWWW_KEY,
+                'webp' => '1',
+                'file' => curl_file_create($source),
+                'domain' => $_SERVER['HTTP_HOST'],
+                'quality' => $quality,
+                'metadata' => ($stripMetadata ? '0' : '1')
+            );
+
+            curl_setopt_array($ch, array(
+                CURLOPT_URL => "https://optimize.exactlywww.com/v2/",
+                CURLOPT_HTTPHEADER => array(
+                    'User-Agent: WebPConvert',
+                    'Accept: image/*'
+                ),
+                CURLOPT_POSTFIELDS => $curlOptions,
+                CURLOPT_BINARYTRANSFER => true,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_HEADER => false,
+                CURLOPT_SSL_VERIFYPEER => false
+            ));
+
+            $response = curl_exec($ch);
+
+            if (curl_errno($ch)) {
+                throw new \Exception(curl_error($ch));
+            }
         } catch (\Exception $e) {
             return false; // TODO: `throw` custom \Exception $e & handle it smoothly on top-level.
         }
 
-        $curlOptions = array(
-            'api_key' => WEBPCONVERT_EWWW_KEY,
-            'webp' => '1',
-            'file' => curl_file_create($source),
-            'domain' => $_SERVER['HTTP_HOST'],
-            'quality' => $quality,
-            'metadata' => ($stripMetadata ? '0' : '1'),
-        );
-
-        curl_setopt_array($curlInit, array(
-            CURLOPT_URL => 'https://optimize.exactlywww.com/v2/',
-            CURLOPT_HTTPHEADER => array(
-                'User-Agent: WebPConvert',
-                'Accept: image/*'
-            ),
-            CURLOPT_POSTFIELDS => $curlOptions,
-            CURLOPT_BINARYTRANSFER => true,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_HEADER => false,
-            CURLOPT_SSL_VERIFYPEER => false
-        ));
-
-        $response = curl_exec($curlInit);
-        $error = curl_error($curlInit);
-
-        curl_close($curlInit);
+        curl_close($ch);
 
         $success = file_put_contents($destination, $response);
 
-        if (!$success || !empty($error)) {
+        if (!$success) {
             return false;
         }
 
