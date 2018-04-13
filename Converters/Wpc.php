9c719497f30b0be59162be707e52483d74eca489
rosell-dk <it@rosell.dk>
2018-04-13 15:20:39 +0200

    created. It needs polishing, but it works

---
+++ Converters/Wpc.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace WebPConvert\Converters;
+
+use WebPConvert\Converters\Exceptions\ConverterNotOperationalException;
+use WebPConvert\Converters\Exceptions\ConverterFailedException;
+
+class Wpc
+{
+    public static function convert($source, $destination, $options = array(), $prepareDestinationFolder = true)
+    {
+        if ($prepareDestinationFolder) {
+            ConverterHelper::prepareDestinationFolderAndRunCommonValidations($source, $destination);
+        }
+
+        $defaultOptions = array(
+            'url' => '',
+            'quality' => 80,
+            'metadata' => 'none',
+        );
+
+        $options = array_merge($defaultOptions, $options);
+
+        if ($options['url'] == '') {
+            throw new ConverterNotOperationalException('Missing URL. You must install WebpConvertCloudService on a server, and supply url');
+        }
+
+        if (!extension_loaded('curl')) {
+            throw new ConverterNotOperationalException('Required cURL extension is not available.');
+        }
+
+        if (!function_exists('curl_init')) {
+            throw new ConverterNotOperationalException('Required url_init() function is not available.');
+        }
+
+        if (!function_exists('curl_file_create')) {
+            throw new ConverterNotOperationalException('Required curl_file_create() function is not available (requires PHP > 5.5).');
+        }
+
+        // Got some code here:
+        // https://coderwall.com/p/v4ps1a/send-a-file-via-post-with-curl-and-php
+
+        $ch = curl_init();
+        if (!$ch) {
+            throw new ConverterNotOperationalException('Could not initialise cURL.');
+        }
+
+        curl_setopt_array($ch, array(
+            CURLOPT_URL => $options['url'],
+            CURLOPT_POST => 1,
+            CURLOPT_POSTFIELDS => array(
+                'quality' => $options['quality'],
+                'file' => curl_file_create($source),
+            ),
+            CURLOPT_BINARYTRANSFER => true,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_HEADER => false,
+            CURLOPT_SSL_VERIFYPEER => false
+        ));
+
+        $response = curl_exec($ch);
+
+        if (curl_errno($ch)) {
+            throw new ConverterNotOperationalException(curl_error($ch));
+        }
+
+        // The WPC cloud service either returns an image or an error message
+        // Images has application/octet-stream.
+
+        // Verify that we got an image back.
+        if (curl_getinfo($ch, CURLINFO_CONTENT_TYPE) != 'application/octet-stream') {
+            curl_close($ch);
+            throw new ConverterFailedException($response);
+            //throw new ConverterNotOperationalException($response);
+        }
+
+        $success = file_put_contents($destination, $response);
+        curl_close($ch);
+
+        if (!$success) {
+            throw new ConverterFailedException('Error saving file');
+        }
+/*
+        $curlOptions = [
+            'api_key' => $options['key'],
+            'webp' => '1',
+            'file' => curl_file_create($source),
+            'domain' => $_SERVER['HTTP_HOST'],
+            'quality' => $options['quality'],
+            'metadata' => ($options['metadata'] == 'none' ? '0' : '1')
+        ];
+
+        curl_setopt_array($ch, [
+            CURLOPT_URL => "https://optimize.exactlywww.com/v2/",
+            CURLOPT_HTTPHEADER => [
+                'User-Agent: WebPConvert',
+                'Accept: image/*'
+            ],
+            CURLOPT_POSTFIELDS => $curlOptions,
+            CURLOPT_BINARYTRANSFER => true,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_HEADER => false,
+            CURLOPT_SSL_VERIFYPEER => false
+        ]);*/
+    }
+}
