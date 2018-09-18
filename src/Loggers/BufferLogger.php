6c49347105ec4cd3226d616515ddb1ba8f61a4a9
rosell-dk <it@rosell.dk>
2018-09-18 20:23:01 +0200

    Merging webp-convert-and-serve project into webp-convert. The BufferLogger was needed there, and is now needed here

---
+++ src/Loggers/BufferLogger.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace WebPConvert\Loggers;
+
+use WebPConvert\Loggers\BaseLogger;
+
+class BufferLogger extends BaseLogger
+{
+    public $entries = array();
+
+    public function log($msg, $style = '')
+    {
+        $this->entries[] = [$msg, $style];
+    }
+
+    public function ln()
+    {
+        $this->entries[] = '';
+    }
+
+    public function getHtml()
+    {
+        $html = '';
+        foreach ($this->entries as $entry) {
+            if ($entry == '') {
+                $html .= '<br>';
+            } else {
+                list($msg, $style) = $entry;
+
+                if ($style == 'bold') {
+                    $html .= '<b>' . $msg . '</b>';
+                } elseif ($style == 'italic') {
+                    $html .= '<i>' . $msg . '</i>';
+                } else {
+                    $html .= $msg;
+                }
+            }
+        }
+        return $html;
+    }
+
+    public function getText($newLineChar = ' ')
+    {
+        $text = '';
+        foreach ($this->entries as $entry) {
+            if ($entry == '') {
+                if (substr($text, -2) != '. ') {
+                    $text .= '. ';
+                }
+            } else {
+                list($msg, $style) = $entry;
+                $text .= $msg;
+            }
+        }
+
+        return $text;
+    }
+}
