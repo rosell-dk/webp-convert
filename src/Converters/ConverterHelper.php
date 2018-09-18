f551ab077fb31556fbb05f9d79ec2202b3765a2d
rosell-dk <it@rosell.dk>
2018-09-18 20:12:48 +0200

    Added runConverterStack function (moved that functionality from WebpConvert.php to here, to make WebpConvert.php ultra-light)

--- src/Converters/ConverterHelper.php
+++ src/Converters/ConverterHelper.php
@@ -45,7 +45,7 @@ class ConverterHelper
     public static function runConverter($converterId, $source, $destination, $options = [], $prepareDestinationFolder = true, $logger = null)
     {
         if ($prepareDestinationFolder) {
-            ConverterHelper::prepareDestinationFolderAndRunCommonValidations($source, $destination);
+            self::prepareDestinationFolderAndRunCommonValidations($source, $destination);
         }
 
         if (!isset($logger)) {
@@ -84,6 +84,119 @@ class ConverterHelper
         if (!file_exists($destination)) {
             throw new ConverterFailedException('Destination file is not there');
         }
+    }
+
+    /*
+      @param (string) $source: Absolute path to image to be converted (no backslashes). Image must be jpeg or png
+      @param (string) $destination: Absolute path (no backslashes)
+      @param (object) $options: Array of named options, such as 'quality' and 'metadata'
+    */
+    public static function runConverterStack($source, $destination, $options = [], $logger = null)
+    {
+        if (!isset($logger)) {
+            $logger = new \WebPConvert\Loggers\VoidLogger();
+        }
+        self::prepareDestinationFolderAndRunCommonValidations($source, $destination);
+
+        $options = array_merge(self::$defaultOptions, $options);
+
+        self::processQualityOption($source, $options, $logger);
+
+        // Force lossless option to true for PNG images
+        if (self::getExtension($source) == 'png') {
+            $options['lossless'] = true;
+        }
+
+        $defaultConverterOptions = $options;
+        $defaultConverterOptions['converters'] = null;
+
+        $firstFailException = null;
+
+        // If we have set converter options for a converter, which is not in the converter array,
+        // then we add it to the array
+        if (isset($options['converter-options'])) {
+            foreach ($options['converter-options'] as $converterName => $converterOptions) {
+                if (!in_array($converterName, $options['converters'])) {
+                    $options['converters'][] = $converterName;
+                }
+            }
+        }
+
+        foreach ($options['converters'] as $converter) {
+            if (is_array($converter)) {
+                $converterId = $converter['converter'];
+                $converterOptions = $converter['options'];
+            } else {
+                $converterId = $converter;
+                $converterOptions = [];
+                if (isset($options['converter-options'][$converterId])) {
+                    // Note: right now, converter-options are not meant to be used, when you have several converters of the same type
+                    $converterOptions = $options['converter-options'][$converterId];
+                }
+            }
+
+            $converterOptions = array_merge($defaultConverterOptions, $converterOptions);
+
+            try {
+                $logger->logLn('Trying:' . $converterId, 'italic');
+
+                // If quality is different, we must recalculate
+                if ($converterOptions['quality'] != $defaultConverterOptions['quality']) {
+                    unset($converterOptions['_calculated_quality']);
+                    self::processQualityOption($source, $converterOptions, $logger);
+                }
+
+                self::runConverter($converterId, $source, $destination, $converterOptions, false, $logger);
+
+                // Still here? - well, we did it! - job is done.
+                $logger->logLn('ok', 'bold');
+                return true;
+            } catch (\WebPConvert\Converters\Exceptions\ConverterNotOperationalException $e) {
+//                $logger->logLnLn($e->description . ' : ' . $e->getMessage());
+                $logger->logLnLn($e->getMessage());
+
+                // The converter is not operational.
+                // Well, well, we will just have to try the next, then
+            } catch (\WebPConvert\Converters\Exceptions\ConverterFailedException $e) {
+                $logger->logLnLn($e->getMessage());
+
+                // Converter failed in an anticipated, yet somewhat surprising fashion.
+                // The converter seemed operational - requirements was in order - but it failed anyway.
+                // This is moderately bad.
+                // If some other converter can handle the conversion, we will let this one go.
+                // But if not, we shall throw the exception
+
+                if (!$firstFailException) {
+                    $firstFailException = $e;
+                }
+            } catch (\WebPConvert\Converters\Exceptions\ConversionDeclinedException $e) {
+                $logger->logLnLn($e->getMessage());
+
+                // The converter declined.
+                // Gd is for example throwing this, when asked to convert a PNG, but configured not to
+                // We also possibly rethrow this, because it may have come as a surprise to the user
+                // who perhaps only tested jpg
+                if (!$firstFailException) {
+                    $firstFailException = $e;
+                }
+            }
+        }
+
+        if ($firstFailException) {
+            // At least one converter failed or declined.
+            $logger->logLn('Conversion failed. None of the tried converters could convert the image', 'bold');
+        } else {
+            // All converters threw a ConverterNotOperationalException
+            $logger->logLn('Conversion failed. None of the tried converters are operational', 'bold');
+        }
+
+        // No converters could do the job.
+        // If one of them failed moderately bad, rethrow that exception.
+        if ($firstFailException) {
+            throw $firstFailException;
+        }
+
+        return false;
     }
 
     /* Try to detect quality of jpeg.
