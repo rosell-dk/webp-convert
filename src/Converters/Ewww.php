218738d2e539c05006fd9a724880ad2adf7533e3
rosell-dk <it@rosell.dk>
2018-09-24 11:15:34 +0200

    added parseSize method

--- src/Converters/Ewww.php
+++ src/Converters/Ewww.php
@@ -22,6 +22,20 @@ class Ewww
         ConverterHelper::runConverter('ewww', $source, $destination, $options, true);
     }
 
+    // Took this parser from Drupal
+    private static function parseSize($size) {
+
+        $unit = preg_replace('/[^bkmgtpezy]/i', '', $size); // Remove the non-unit characters from the size.
+            $size = preg_replace('/[^0-9\.]/', '', $size); // Remove the non-numeric characters from the size.
+            if ($unit) {
+                // Find the position of the unit in the ordered string which is the power of magnitude to multiply a kilobyte by.
+                return round($size * pow(1024, stripos('bkmgtpezy', $unit[0])));
+            }
+            else {
+                return round($size);
+            }
+    }
+
     // Although this method is public, do not call directly.
     public static function doConvert($source, $destination, $options, $logger)
     {
