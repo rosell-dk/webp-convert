df11f06a516d6f508db570830160568a53097407
rosell-dk <it@rosell.dk>
2018-09-24 14:09:45 +0200

    cs-fix

--- src/Converters/Ewww.php
+++ src/Converters/Ewww.php
@@ -23,17 +23,18 @@ class Ewww
     }
 
     // Took this parser from Drupal
-    private static function parseSize($size) {
+    private static function parseSize($size)
+    {
 
         $unit = preg_replace('/[^bkmgtpezy]/i', '', $size); // Remove the non-unit characters from the size.
             $size = preg_replace('/[^0-9\.]/', '', $size); // Remove the non-numeric characters from the size.
-            if ($unit) {
-                // Find the position of the unit in the ordered string which is the power of magnitude to multiply a kilobyte by.
-                return round($size * pow(1024, stripos('bkmgtpezy', $unit[0])));
-            }
-            else {
-                return round($size);
-            }
+        if ($unit) {
+            // Find the position of the unit in the ordered string which is the power
+            // of magnitude to multiply a kilobyte by.
+            return round($size * pow(1024, stripos('bkmgtpezy', $unit[0])));
+        } else {
+            return round($size);
+        }
     }
 
     // Although this method is public, do not call directly.
@@ -83,7 +84,6 @@ class Ewww
             }
 
             // ini_get('memory_limit')
-
         }
 
 
