b6370fc7a6d44e8fba13efe5e557c1c488836b54
rosell-dk <it@rosell.dk>
2018-11-16 16:44:21 +0100

    new testFilesizeRequirements() method, taken from ewww (and wpc)

--- src/Convert/CloudConverter.php
+++ src/Convert/CloudConverter.php
@@ -34,4 +34,48 @@ class CloudConverter extends BaseConverter
         }
         return $ch;
     }
+
+    // Parse size found in php.ini
+    // Took the parser from Drupal
+    public static function parseSize($size)
+    {
+
+        $unit = preg_replace('/[^bkmgtpezy]/i', '', $size); // Remove the non-unit characters from the size.
+            $size = preg_replace('/[^0-9\.]/', '', $size); // Remove the non-numeric characters from the size.
+        if ($unit) {
+            // Find the position of the unit in the ordered string which is the power
+            // of magnitude to multiply a kilobyte by.
+            return round($size * pow(1024, stripos('bkmgtpezy', $unit[0])));
+        } else {
+            return round($size);
+        }
+    }
+
+    public function testFilesizeRequirements()
+    {
+        $fileSize = @filesize($this->source);
+        if ($fileSize !== false) {
+            $uploadMaxSize = self::parseSize(ini_get('upload_max_filesize'));
+            if (($uploadMaxSize !== false) && ($uploadMaxSize < $fileSize)) {
+                throw new ConverterFailedException(
+                    'File is larger than your max upload (set in your php.ini). File size:' .
+                        round($fileSize/1024) . ' kb. ' .
+                        'upload_max_filesize in php.ini: ' . ini_get('upload_max_filesize') .
+                        ' (parsed as ' . round($uploadMaxSize/1024) . ' kb)'
+                );
+            }
+
+            $postMaxSize = self::parseSize(ini_get('post_max_size'));
+            if (($postMaxSize !== false) && ($postMaxSize < $fileSize)) {
+                throw new ConverterFailedException(
+                    'File is larger than your post_max_size limit (set in your php.ini). File size:' .
+                        round($fileSize/1024) . ' kb. ' .
+                        'post_max_size in php.ini: ' . ini_get('post_max_size') .
+                        ' (parsed as ' . round($postMaxSize/1024) . ' kb)'
+                );
+            }
+
+            // ini_get('memory_limit')
+        }
+    }
 }
