3031e803ee4e67408727cd4207f7de89b4124ed0
rosell-dk <it@rosell.dk>
2019-03-06 10:20:21 +0100

    Fixed merge error

--- src/Converters/Imagickbinary.php
+++ src/Converters/Imagickbinary.php
@@ -1,18 +1,14 @@
 <?php
-
 // TODO: Quality option
-
 namespace WebPConvert\Converters;
 
 use WebPConvert\Converters\Exceptions\ConverterNotOperationalException;
 use WebPConvert\Converters\Exceptions\ConverterFailedException;
-use WebPConvert\Convert\ExecConverter;
-
 //use WebPConvert\Exceptions\TargetNotFoundException;
+use WebPConvert\Convert\ExecConverter;
 
 // To futher improve this converter, I could check out:
 // https://github.com/Orbitale/ImageMagickPHP
-
 class ImagickBinary extends ExecConverter
 {
     public static $extraOptions = [
@@ -24,16 +20,12 @@ class ImagickBinary extends ExecConverter
             'required' => false
         ],
     ];
-
-
     //public $id = 'imagickbinary';
-
     public static function imagickInstalled()
     {
         exec('convert -version', $output, $returnCode);
         return ($returnCode == 0);
     }
-
     // Check if webp delegate is installed
     public static function webPDelegateInstalled()
     {
@@ -42,7 +34,6 @@ class ImagickBinary extends ExecConverter
         exec($command, $output, $returnCode);
         return (count($output) > 0);
         */
-
         $command = 'convert -version';
         exec($command, $output, $returnCode);
         $hasDelegate = false;
@@ -53,7 +44,6 @@ class ImagickBinary extends ExecConverter
         }
         return false;
     }
-
     // Although this method is public, do not call directly.
     // You should rather call the static convert() function, defined in BaseConverter, which
     // takes care of preparing stuff before calling doConvert, and validating after.
@@ -62,48 +52,32 @@ class ImagickBinary extends ExecConverter
         if (!self::imagickInstalled()) {
             throw new ConverterNotOperationalException('imagick is not installed');
         }
-
-
-        // TODO:
-        // quality. Like this: 'convert -quality 100 small.jpg small.webp'
-        $qualityOption = '';
-        //$this->logLn('Using quality:' . $this->getCalculatedQuality());
-        if (isset($options['_quality_could_not_be_detected'])) {
-            // quality was set to "auto", but we could not meassure the quality of the jpeg locally
-            // but luckily imagick is a big boy, and automatically converts with same quality as
-            // source, when the quality isn't set.
-        } else {
-            $qualityOption = '-quality ' . $options['_calculated_quality'] . ' ';
+        if (!self::webPDelegateInstalled()) {
+            throw new ConverterNotOperationalException('webp delegate missing');
         }
-
         //$this->logLn('Using quality:' . $this->getCalculatedQuality());
-
         // Should we use "magick" or "convert" command?
         // It seems they do the same. But which is best supported? Which is mostly available (whitelisted)?
         // Should we perhaps try both?
         // For now, we just go with "convert"
-        $command = 'convert ' . self::escapeFilename($this->source) . ' webp:' . self::escapeFilename($this->destination);
+        $command = 'convert ' .
+            self::escapeFilename($this->source) . ' webp:' . self::escapeFilename($this->destination);
 
         // TODO:
         // quality. Like this: 'convert -quality 100 small.jpg small.webp'
-
         $useNice = (($this->options['use-nice']) && self::hasNiceSupport()) ? true : false;
         if ($useNice) {
             $this->logLn('using nice');
             $command = 'nice ' . $command;
         }
-
         exec($command, $output, $returnCode);
-
         if ($returnCode == 127) {
             throw new ConverterNotOperationalException('imagick is not installed');
         }
-
         if ($returnCode != 0) {
             $this->logLn('command:' . $command);
             $this->logLn('return code:' . $returnCode);
             $this->logLn('output:' . print_r(implode("\n", $output), true));
-
             throw new ConverterNotOperationalException('The exec call failed');
         }
     }
