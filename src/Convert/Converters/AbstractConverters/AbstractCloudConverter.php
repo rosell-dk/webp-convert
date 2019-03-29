2006e9af144e2bd1ada8af104dadc6b132e15948
rosell-dk <it@rosell.dk>
2019-03-29 22:32:38 +0100

    Improved AbstractCloudConverter and its tests

--- src/Convert/Converters/AbstractConverters/AbstractCloudConverter.php
+++ src/Convert/Converters/AbstractConverters/AbstractCloudConverter.php
@@ -8,6 +8,12 @@ use WebPConvert\Convert\Converters\AbstractConverters\AbstractConverter;
 
 abstract class AbstractCloudConverter extends AbstractConverter
 {
+    /**
+     *  Test if basic requirements are met for using curl.
+     *
+     *  @throws SystemRequirementsNotMetException  if requirements are not met.
+     *  @return void
+     */
     public static function testCurlRequirements()
     {
         if (!extension_loaded('curl')) {
@@ -25,6 +31,12 @@ abstract class AbstractCloudConverter extends AbstractConverter
         }
     }
 
+    /**
+     *  Init curl.
+     *
+     * @throws  SystemRequirementsNotMetException  if curl could not be initialized
+     * @return  resource  curl handle
+     */
     public static function initCurl()
     {
         // Get curl handle
@@ -35,28 +47,73 @@ abstract class AbstractCloudConverter extends AbstractConverter
         return $ch;
     }
 
-    // Parse size found in php.ini
-    // Took the parser from Drupal
-    public static function parseSize($size)
+    /**
+     * Parse a shordhandsize string as the ones returned by ini_get()
+     *
+     * Parse a shorthandsize string having the syntax allowed in php.ini and returned by ini_get().
+     * Ie "1K" => 1024.
+     * Strings without units are also accepted.
+     * The shorthandbytes syntax is described here: https://www.php.net/manual/en/faq.using.php#faq.using.shorthandbytes
+     *
+     * @param  string  $size  A size string of the type returned by ini_get()
+     * @return float|false  The parsed size (beware: it is float, do not check high numbers for equality), or false if parse error
+     */
+    public static function parseShortHandSize($shortHandSize)
     {
 
-        $unit = preg_replace('/[^bkmgtpezy]/i', '', $size); // Remove the non-unit characters from the size.
-            $size = preg_replace('/[^0-9\.]/', '', $size); // Remove the non-numeric characters from the size.
-        if ($unit) {
+        $result = preg_match("#^\\s*(\\d+(?:\\.\\d+)?)([bkmgtpezy]?)\\s*$#i", $shortHandSize, $matches);
+        if ($result !== 1) {
+            return false;
+        }
+
+        // Truncate, because that is what php does.
+        $digitsValue = floor($matches[1]);
+
+        if ((count($matches) >= 3) && ($matches[2] != '')) {
+            $unit = $matches[2];
+
             // Find the position of the unit in the ordered string which is the power
             // of magnitude to multiply a kilobyte by.
-            return round($size * pow(1024, stripos('bkmgtpezy', $unit[0])));
+            $position = stripos('bkmgtpezy', $unit);
+
+            return floatval($digitsValue * pow(1024, $position));
         } else {
-            return round($size);
+            return $digitsValue;
         }
     }
 
+    /*
+    * Get the size of an php.ini option.
+    *
+    * Calls ini_get() and parses the size to a number.
+    * If the configuration option is null, does not exist, or cannot be parsed as a shorthandsize, false is returned
+    *
+    * @param  string  $varname  The configuration option name.
+    * @return float|false  The parsed size or false if the configuration option does not exist
+    */
+    public static function getIniBytes($iniVarName)
+    {
+        $iniVarValue = ini_get($iniVarName);
+        if (($iniVarValue == '') || $iniVarValue === false) {
+            return false;
+        }
+        return self::parseShortHandSize($iniVarValue);
+    }
+
+    /**
+     *  Test that filesize is below "upload_max_filesize" and "post_max_size" values in php.ini
+     *
+     * @throws  ConversionFailedException  if filesize is too large
+     * @return  void
+     */
     public function testFilesizeRequirements()
     {
         $fileSize = @filesize($this->source);
         if ($fileSize !== false) {
-            $uploadMaxSize = self::parseSize(ini_get('upload_max_filesize'));
-            if (($uploadMaxSize !== false) && ($uploadMaxSize < $fileSize)) {
+            $uploadMaxSize = self::getIniBytes('upload_max_filesize');
+            if ($uploadMaxSize === false) {
+                // Not sure if we should throw an exception here, or not...
+            } elseif ($uploadMaxSize < $fileSize) {
                 throw new ConversionFailedException(
                     'File is larger than your max upload (set in your php.ini). File size:' .
                         round($fileSize/1024) . ' kb. ' .
@@ -65,8 +122,10 @@ abstract class AbstractCloudConverter extends AbstractConverter
                 );
             }
 
-            $postMaxSize = self::parseSize(ini_get('post_max_size'));
-            if (($postMaxSize !== false) && ($postMaxSize < $fileSize)) {
+            $postMaxSize = self::getIniBytes(ini_get('post_max_size'));
+            if ($postMaxSize === false) {
+                // Not sure if we should throw an exception here, or not...
+            } elseif ($postMaxSize < $fileSize) {
                 throw new ConversionFailedException(
                     'File is larger than your post_max_size limit (set in your php.ini). File size:' .
                         round($fileSize/1024) . ' kb. ' .
@@ -75,6 +134,7 @@ abstract class AbstractCloudConverter extends AbstractConverter
                 );
             }
 
+            // Should we worry about memory limit as well?
             // ini_get('memory_limit')
         }
     }
