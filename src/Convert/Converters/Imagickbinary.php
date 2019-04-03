4e995991d94fb5040a09db4e1bb85bf5da7ce02b
rosell-dk <it@rosell.dk>
2019-04-03 12:31:33 +0200

    Added quality option for ImagickBinary

--- src/Convert/Converters/Imagickbinary.php
+++ src/Convert/Converters/Imagickbinary.php
@@ -23,6 +23,31 @@ class ImagickBinary extends AbstractExecConverter
         ],
     ];
 
+    public static function imagickInstalled()
+    {
+        exec('convert -version', $output, $returnCode);
+        return ($returnCode == 0);
+    }
+
+    // Check if webp delegate is installed
+    public static function webPDelegateInstalled()
+    {
+        /* HM. We should not rely on grep being available
+        $command = 'convert -list configure | grep -i "delegates" | grep -i webp';
+        exec($command, $output, $returnCode);
+        return (count($output) > 0);
+        */
+        $command = 'convert -version';
+        exec($command, $output, $returnCode);
+        $hasDelegate = false;
+        foreach ($output as $line) {
+            if (preg_match('/Delegate.*webp.*/i', $line)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     /**
      * Check (general) operationality of imagack converter executable
      *
@@ -45,17 +70,23 @@ class ImagickBinary extends AbstractExecConverter
         // It seems they do the same. But which is best supported? Which is mostly available (whitelisted)?
         // Should we perhaps try both?
         // For now, we just go with "convert"
-        $command = 'convert ' .
-            escapeshellarg($this->source) . ' ' . escapeshellarg('webp:' . $this->destination);
-            //self::escapeFilename($this->source) . ' webp:' . self::escapeFilename($this->destination);
 
-        // TODO:
-        // quality. Like this: 'convert -quality 100 small.jpg small.webp'
+
+        $commandArguments = [];
+        if (!$this->isQualitySetToAutoAndDidQualityDetectionFail()) {
+            $commandArguments[] = '-quality ' . escapeshellarg($this->getCalculatedQuality());
+        }
+        $commandArguments[] = escapeshellarg($this->source);
+        $commandArguments[] = escapeshellarg('webp:' . $this->destination);
+
+        $command = 'convert ' . implode(' ', $commandArguments);
+
         $useNice = (($this->options['use-nice']) && self::hasNiceSupport()) ? true : false;
         if ($useNice) {
             $this->logLn('using nice');
             $command = 'nice ' . $command;
         }
+        $this->logLn('command: ' . $command);
         exec($command, $output, $returnCode);
         if ($returnCode == 127) {
             throw new SystemRequirementsNotMetException('imagick is not installed');
@@ -66,30 +97,5 @@ class ImagickBinary extends AbstractExecConverter
             $this->logLn('output:' . print_r(implode("\n", $output), true));
             throw new SystemRequirementsNotMetException('The exec call failed');
         }
-    }
-
-    public static function imagickInstalled()
-    {
-        exec('convert -version', $output, $returnCode);
-        return ($returnCode == 0);
-    }
-    
-    // Check if webp delegate is installed
-    public static function webPDelegateInstalled()
-    {
-        /* HM. We should not rely on grep being available
-        $command = 'convert -list configure | grep -i "delegates" | grep -i webp';
-        exec($command, $output, $returnCode);
-        return (count($output) > 0);
-        */
-        $command = 'convert -version';
-        exec($command, $output, $returnCode);
-        $hasDelegate = false;
-        foreach ($output as $line) {
-            if (preg_match('/Delegate.*webp.*/i', $line)) {
-                return true;
-            }
-        }
-        return false;
     }
 }
