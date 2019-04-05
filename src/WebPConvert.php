038270ed5fbd0adc9dd77e092bef219fa08a479a
rosell-dk <it@rosell.dk>
2019-04-05 09:38:23 +0200

    convert returns void now

--- src/WebPConvert.php
+++ src/WebPConvert.php
@@ -9,16 +9,20 @@ use WebPConvert\Serve\ServeExistingOrHandOver;
 class WebPConvert
 {
 
-    /*
-      @param (string) $source: Absolute path to image to be converted (no backslashes). Image must be jpeg or png
-      @param (string) $destination: Absolute path (no backslashes)
-      @param (object) $options: Array of named options, such as 'quality' and 'metadata'
+    /**
+     * Convert jpeg or png into webp
+     *
+     * @param  string  $source  Absolute path to image to be converted (no backslashes). Image must be jpeg or png
+     * @param  string  $destination  Absolute path (no backslashes)
+     * @param  object  $options  Array of named options, such as 'quality' and 'metadata'
+     * @throws \WebPConvert\Exceptions\WebPConvertException
+     * @return void
     */
     public static function convert($source, $destination, $options = [], $logger = null)
     {
         //return ConverterHelper::runConverterStack($source, $destination, $options, $logger);
         //return Convert::runConverterStack($source, $destination, $options, $logger);
-        return Stack::convert($source, $destination, $options, $logger);
+        Stack::convert($source, $destination, $options, $logger);
     }
 
     public static function convertAndServe($source, $destination, $options = [])
