3731a7d891da0dad53e02633ec239eeed6ee3a96
rosell-dk <it@rosell.dk>
2019-04-06 20:16:10 +0200

    getText() now uses the $newLine parameter

--- src/Loggers/BufferLogger.php
+++ src/Loggers/BufferLogger.php
@@ -43,9 +43,10 @@ class BufferLogger extends BaseLogger
     {
         $text = '';
         foreach ($this->entries as $entry) {
-            if ($entry == '') {
-                if (substr($text, -2) != '. ') {
-                    $text .= '. ';
+            if ($entry == '') {  // empty string means new line
+
+                if (substr($text, -2) != '.' . $newLineChar) {
+                    $text .= '.' . $newLineChar;
                 }
             } else {
                 list($msg, $style) = $entry;
