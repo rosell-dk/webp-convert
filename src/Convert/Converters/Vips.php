ff7b18ca9920e37114f2812295f6f0595849bf91
rosell-dk <it@rosell.dk>
2019-04-23 20:19:53 +0200

    All supported webp options are now in vips. And it now handles when a property isn't supported (by unsetting that property and trying again). #129. #133

--- src/Convert/Converters/Vips.php
+++ src/Convert/Converters/Vips.php
@@ -6,13 +6,20 @@ use WebPConvert\Convert\BaseConverters\AbstractConverter;
 use WebPConvert\Convert\Exceptions\ConversionFailedException;
 use WebPConvert\Convert\Exceptions\ConversionFailed\ConverterNotOperational\SystemRequirementsNotMetException;
 
+//require '/home/rosell/.composer/vendor/autoload.php';
+
 class Vips extends AbstractConverter
 {
     protected $supportsLossless = true;
 
     protected function getOptionDefinitionsExtra()
     {
-        return [];
+        return [
+            ['smart-subsample', 'boolean', false],
+            ['alpha-quality', 'int', 100],        // alpha quality in lossless mode
+            ['near-lossless', 'boolean', false],  // apply near-lossless preprocessing (controled by setting quality to 20,40,60 or 80)
+            ['preset', 'int', 0],                 // preset. 0:default, 1:picture, 2:photo, 3:drawing, 4:icon, 5:text, 6:last
+        ];
     }
 
     /**
@@ -48,21 +55,23 @@ class Vips extends AbstractConverter
         // - so nothing needs to be done here
 
         if (function_exists('vips_version')) {
-            $this->logLn('vips version: ' . vips_version());
+            $this->logLn('vipslib version: ' . vips_version());
         }
+        $this->logLn('vips extension version: ' . phpversion('vips'));
     }
 
     protected function doActualConvert()
     {
-        /*
-        $im = \Jcupitt\Vips\Image::newFromFile(__DIR__ . '/images/small.jpg');
+/*
+        $im = \Jcupitt\Vips\Image::newFromFile($this->source);
         //$im->writeToFile(__DIR__ . '/images/small-vips.webp', ["Q" => 10]);
-        $im->webpsave(__DIR__ . '/images/small-vips.webp', [
+
+        $im->webpsave($this->destination, [
             "Q" => 80,
-            'near_lossless' => true
+            //'near_lossless' => true
         ]);
-        return;
-        */
+        return;*/
+
 
 
         // We are currently using vips_image_new_from_file(), but we could consider
@@ -91,30 +100,56 @@ class Vips extends AbstractConverter
 
         $im = array_shift($result);
 
-        //$this->logLn('lossless:' . ($this->options['lossless'] ? 'yes' : 'no'));
-
-        // for some reason, vips_webpsave function is unavailable on at least one system, so we
-        // use vips_call instead.
-
         // webpsave options are described here:
         // https://jcupitt.github.io/libvips/API/current/VipsForeignSave.html#vips-webpsave
-        $result = vips_call('webpsave', $im, $this->destination, [
+
+        $options = [
             "Q" => $this->getCalculatedQuality(),
             'lossless' => $this->options['lossless'],
-            //'lossless' => $this->options['lossless'],       // boolean
-            //'preset'
-            //'smart_subsample'     // boolean
+            'strip' => $this->options['metadata'] == 'none',
+        ];
 
-            // hm, when I use near_lossless, I get error: "no property named `near_lossless'"
-            // btw: beware that if this is used, q must be 20, 40, 60 or 80 (according to link above)
-            //'near_lossless' => true,   // boolean
+        // Only set the following options if they differ from the default of vipslib
+        // This ensures we do not get warning if that property isn't supported
+        if ($this->options['near-lossless'] !== false) {
+            $options['near_lossless'] = $this->options['near-lossless'];
+        }
+        if ($this->options['smart-subsample'] !== false) {
+            $options['smart_subsample'] = $this->options['smart-subsample'];
+        }
+        if ($this->options['alpha-quality'] !== 100) {
+            $options['alpha_q'] = $this->options['alpha-quality'];
+        }
+        if ($this->options['preset'] !== 0) {
+            $options['preset'] = $this->options['preset'];
+        }
 
-            //'alpha_q'     // int
-            'strip' => $this->options['metadata'] == 'none'
-        ]);
-        if ($result === -1) {
-            $message = vips_error_buffer();
-            throw new ConversionFailedException($message);
+        $done = false;
+
+        // A bit unusual loop.
+        // Iterations happens when vips errors out because of unsupported properties
+        // in that case, we remove that property and try again.
+        while (!$done) {
+            $result = vips_call('webpsave', $im, $this->destination, $options);
+
+            if ($result === -1) {
+                $message = vips_error_buffer();
+
+                // If the error
+                if (preg_match("#no property named .(.*).#", $message, $matches)) {
+                    $nameOfPropertyNotFound = $matches[1];
+                    $this->logLn(
+                        'Your version of vipslib does not support the "' . $nameOfPropertyNotFound . '" property. ' .
+                        'The option is ignored.'
+                    );
+                    unset($options[$nameOfPropertyNotFound]);
+                } else {
+                    throw new ConversionFailedException($message);
+                }
+            } else {
+                $done = true;
+            }
+
         }
     }
 }
