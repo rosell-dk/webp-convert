82f9291e141c159e9f468b1f85d495d24c6834fe
rosell-dk <it@rosell.dk>
2019-04-25 09:38:16 +0200

    test all vips options

--- tests/Convert/Converters/VipsTest.php
+++ tests/Convert/Converters/VipsTest.php
@@ -11,7 +11,14 @@ class VipsTest extends TestCase
 
     public function testConvert()
     {
-        ConverterTestHelper::runAllConvertTests($this, 'Vips');
+        $options = [];
+        ConverterTestHelper::runAllConvertTests($this, 'Vips', $options);
+
+        $options = [
+            'smart-subsample' => true,
+            'preset' => 1,
+        ];
+        ConverterTestHelper::runAllConvertTests($this, 'Vips', $options);
     }
 
     public static $imageDir = __DIR__ . '/../..';
