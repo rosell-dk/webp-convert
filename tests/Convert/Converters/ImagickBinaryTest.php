8f5940f9bdff272828d2cbb729c01983a0c97c75
rosell-dk <it@rosell.dk>
2019-05-02 17:25:17 +0200

    added nice test

--- tests/Convert/Converters/ImagickBinaryTest.php
+++ tests/Convert/Converters/ImagickBinaryTest.php
@@ -9,14 +9,47 @@
 namespace WebPConvert\Tests\Convert\Converters;
 
 use WebPConvert\Convert\Converters\ImagickBinary;
+use WebPConvert\Convert\Exceptions\ConversionFailed\ConverterNotOperationalException;
+use WebPConvert\Loggers\BufferLogger;
+
 use PHPUnit\Framework\TestCase;
 
 class ImagickBinaryTest extends TestCase
 {
 
+    public $imageDir = __DIR__ . '/../../images/';
+
     public function testConvert()
     {
         ConverterTestHelper::runAllConvertTests($this, 'ImagickBinary');
+    }
+
+    private static function tryThis($test, $source, $options)
+    {
+        $bufferLogger = new BufferLogger();
+
+        try {
+            ImagickBinary::convert($source, $source . '.webp', $options, $bufferLogger);
+
+            $test->addToAssertionCount(1);
+        } catch (ConversionFailedException $e) {
+
+            //$bufferLogger->getText()
+            throw $e;
+        } catch (ConverterNotOperationalException $e) {
+            throw $e;
+            // this is ok.
+            return;
+        }
+    }
+
+    public function testWithNice() {
+        $source = $this->imageDir . '/test.png';
+        $options = [
+            'use-nice' => true,
+            'lossless' => true,
+        ];
+        self::tryThis($this, $source, $options);
     }
 
 }
