363661658a69bba8094d6aeb046f2e7b5d1a9e94
rosell-dk <it@rosell.dk>
2019-05-03 11:02:17 +0200

    added testing of native imagick functions

--- tests/Convert/Converters/ImagickTest.php
+++ tests/Convert/Converters/ImagickTest.php
@@ -7,9 +7,35 @@ use PHPUnit\Framework\TestCase;
 class ImagickTest extends TestCase
 {
 
+    public static $imageDir = __DIR__ . '/../../images/';
+
     public function testConvert()
     {
         ConverterTestHelper::runAllConvertTests($this, 'Imagick');
     }
 
+    public function testQueryFormats()
+    {
+        if (!extension_loaded('imagick')) {
+            return;
+        }
+
+        $im = new \Imagick();
+
+        $this->assertEquals(1, count($im->queryFormats('JPEG')));
+        $this->assertGreaterThan(2, count($im->queryFormats('*')));
+        $this->assertGreaterThan(2, count($im->queryFormats()));
+        $this->assertEquals(count($im->queryFormats('*')), count($im->queryFormats()));
+    }
+
+    public function testThatImagickFunctionsUsedDoesNotThrow()
+    {
+        $im = new \Imagick(self::$imageDir . '/test.jpg');
+        $im->setImageFormat('JPEG');
+        $im->stripImage();
+        $im->setImageCompressionQuality(100);
+        $imageBlob = $im->getImageBlob();
+
+        $this->addToAssertionCount(1);
+    }
 }
