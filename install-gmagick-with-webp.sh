9f90e4bd954173eecea9539ef020b658e4d67d11
rosell-dk <it@rosell.dk>
2019-05-03 11:02:51 +0200

    added gmagick install script, with webp

--- install-gmagick-with-webp.sh
+++ install-gmagick-with-webp.sh
@@ -1,12 +1,20 @@
-
+# https://duntuk.com/how-install-graphicsmagick-gmagick-php-extension
 gm -version | grep -i 'WebP.*yes' && {
     echo "Gmagick is already compiled with webp. Nothing to do :)" &&
     echo ":)"
+
 }
 
 gm -version | grep -i 'WebP.*yes' || {
-    echo "Gmagick is not compiled with webp... " &&
-    echo ":("
+    echo "Gmagick is not compiled with webp... Doing that!" &&
+    cd /tmp &&
+    wget ftp://ftp.graphicsmagick.org/pub/GraphicsMagick/GraphicsMagick-LATEST.tar.gz &&
+    tar xvfz GraphicsMagick-LATEST.tar.gz &&
+    cd GraphicsMagick-* &&
+    ./configure --enable-shared --with-webp=yes &&
+    make &&
+    make install &&
+    gm -version
 }
 
 #convert -version | grep 'webp' || {
