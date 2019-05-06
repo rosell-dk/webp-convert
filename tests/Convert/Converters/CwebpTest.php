4af56f8494bf017cca9aac23c7334f50c5ef3f81
rosell-dk <it@rosell.dk>
2019-05-06 21:48:17 +0200

    improved tests

--- tests/Convert/Converters/CwebpTest.php
+++ tests/Convert/Converters/CwebpTest.php
@@ -10,9 +10,18 @@
 namespace WebPConvert\Tests\Convert\Converters;
 
 use WebPConvert\Convert\Converters\Cwebp;
+use WebPConvert\Convert\Exceptions\ConversionFailedException;
+use WebPConvert\Convert\Exceptions\ConversionFailed\ConverterNotOperationalException;
+
 use WebPConvert\Tests\Convert\Exposers\CwebpExposer;
+
 use PHPUnit\Framework\TestCase;
 
+
+/**
+ * @coversDefaultClass WebPConvert\Convert\Converters\Cwebp
+ * @covers WebPConvert\Convert\Converters\Cwebp
+ */
 class CwebpTest extends TestCase
 {
 
@@ -33,14 +42,18 @@ class CwebpTest extends TestCase
         $this->assertTrue(file_exists($source), 'source does not exist');
     }
 
+    /**
+     * @covers ::createCommandLineOptions
+     */
     public function testCreateCommandLineOptions()
     {
         $source = self::$imageDir . '/test.png';
-        $cwebp = new Cwebp($source, $source . '.webp', [
+        $options = [
             'quality' => 'auto',
             'method' => 3,
-            'command-line-options' => '-sharpness 5 -crop 10 10 40 40'
-        ]);
+            'command-line-options' => '-sharpness 5 -crop 10 10 40 40 -low_memory',
+        ];
+        $cwebp = new Cwebp($source, $source . '.webp', $options);
         $cwebpExposer = new CwebpExposer($cwebp);
 
         //$cwebpExposer->prepareOptions();
@@ -63,25 +76,134 @@ class CwebpTest extends TestCase
         // -sharpness '5'
         $this->assertRegExp('#-sharpness \'5\'#', $commandLineOptions);
 
-        // Option with multiple values. Each are escapeshellarg'ed
+        // Extra command line option with multiple values. Each are escapeshellarg'ed
         $this->assertRegExp('#-crop \'10\' \'10\' \'40\' \'40\'#', $commandLineOptions);
 
+        // Command line option (flag)
+        $this->assertRegExp('#-low_memory#', $commandLineOptions);
+
+        // -sharpness '5'
+        $this->assertRegExp('#-sharpness \'5\'#', $commandLineOptions);
     }
 
-/*
+    /**
+     * @covers ::createCommandLineOptions
+     */
     public function testCreateCommandLineOptions2()
     {
         $source = self::$imageDir . '/test.png';
-        $cwebp = new Cwebp($source, $source . '.webp', [
-            'quality' => 'auto',
+        $options = [
+            'quality' => 70,
             'method' => 3,
-        ]);
+            'size-in-percentage' => 55,
+        ];
+        $cwebp = new Cwebp($source, $source . '.webp', $options);
         $cwebpExposer = new CwebpExposer($cwebp);
 
-        $cwebpExposer->prepareOptions();
+        //$cwebpExposer->prepareOptions();
 
         $commandLineOptions = $cwebpExposer->createCommandLineOptions();
-    }*/
+
+        // Size
+        $fileSizeInBytes = floor($options['size-in-percentage']/100 * filesize($source));
+        $this->assertEquals(1714, $fileSizeInBytes);
+        $this->assertRegExp('#-size ' . $fileSizeInBytes . '#', $commandLineOptions);
+
+        // There must be no quality option, because -size overrules it.
+        $this->assertNotRegExp('#-q \\d+#', $commandLineOptions);
+    }
+
+    /**
+     * @covers ::createCommandLineOptions
+     */
+    public function testCreateCommandLineOptions3()
+    {
+        $source = self::$imageDir . '/test.png';
+        $options = [
+            'lossless' => true,
+            'near-lossless' => 75,
+            'autofilter' => true,
+        ];
+        $cwebp = new Cwebp($source, $source . '.webp', $options);
+        $cwebpExposer = new CwebpExposer($cwebp);
+
+        $commandLineOptions = $cwebpExposer->createCommandLineOptions();
+
+        // near-lossless
+        $this->assertRegExp('#-near_lossless 75#', $commandLineOptions);
+
+        // There must be no -lossless option, because -near-lossless overrules it.
+        $this->assertNotRegExp('#-lossless#', $commandLineOptions);
+
+        // autofilter
+        $this->assertRegExp('#-af#', $commandLineOptions);
+
+        // no low-memory
+        $this->assertNotRegExp('#-low_memory#', $commandLineOptions);
+    }
+
+    /**
+     * @covers ::createCommandLineOptions
+     */
+    public function testCreateCommandLineOptions4()
+    {
+        $source = self::$imageDir . '/test.png';
+        $options = [
+            'lossless' => true,
+            'near-lossless' => 100,
+            'low-memory' => true,
+        ];
+        $cwebp = new Cwebp($source, $source . '.webp', $options);
+        $cwebpExposer = new CwebpExposer($cwebp);
+
+        $commandLineOptions = $cwebpExposer->createCommandLineOptions();
+
+        // lossless
+        $this->assertRegExp('#-lossless#', $commandLineOptions);
+
+        // There must be no -near_lossless option, because -lossless overrules it.
+        $this->assertNotRegExp('#-near_lossless#', $commandLineOptions);
+
+        // low-memory
+        $this->assertRegExp('#-low_memory#', $commandLineOptions);
+
+        // No autofilter
+        $this->assertNotRegExp('#-af#', $commandLineOptions);
+    }
+
+    /**
+     * @covers ::checkOperationality
+     */
+     public function testOperatinalityException()
+     {
+         $source = self::$imageDir . '/test.png';
+         $options = [
+             'try-supplied-binary-for-os' => false,
+             'try-common-system-paths' => false,
+         ];
+         $this->expectException(ConverterNotOperationalException::class);
+         //$cwebp = new Cwebp($source, $source . '.webp', $options);
+         Cwebp::convert($source, $source . '.webp', $options);
+     }
+
+     public function testUsingSuppliedBinaryForOS()
+     {
+         $source = self::$imageDir . '/test.png';
+         $options = [
+             'try-supplied-binary-for-os' => true,
+             'try-common-system-paths' => false,
+         ];
+         //$this->expectException(ConverterNotOperationalException::class);
+         //$cwebp = new Cwebp($source, $source . '.webp', $options);
+         try {
+             Cwebp::convert($source, $source . '.webp', $options);
+         } catch (ConversionFailedException $e) {
+             // this is ok.
+             // - but other exceptions are not!
+         }
+         $this->addToAssertionCount(1);
+
+     }
 
   /*
     public function testCwebpDefaultPaths()
