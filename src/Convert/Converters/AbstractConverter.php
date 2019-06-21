5b315745e8c3c1eca3b898471abaa933a4a41681
rosell-dk <it@rosell.dk>
2019-06-21 13:15:24 +0200

    2.1.1

--- src/Convert/Converters/AbstractConverter.php
+++ src/Convert/Converters/AbstractConverter.php
@@ -117,7 +117,7 @@ abstract class AbstractConverter
         $this->setProvidedOptions($options);
 
         if (!isset($this->options['_skip_input_check'])) {
-            $this->log('WebP Convert 2.0.5', 'italic');
+            $this->log('WebP Convert 2.1.1', 'italic');
             $this->logLn(' ignited.');
             $this->logLn('- PHP version: ' . phpversion());
             if (isset($_SERVER['SERVER_SOFTWARE'])) {
