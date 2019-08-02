b28ce7fe71ce4f930bd352f12b4f1591ac21b9f0
rosell-dk <it@rosell.dk>
2019-08-02 16:13:24 +0200

    Quality detection could sometimes return 0 (when Imagick could not detect the quality). This is now treated as a failure. Also, graphicsmagick seems to always return 75, and that is now treated as a failure to detect. Fixes 208. Fixes 209.

--- src/Convert/Helpers/JpegQualityDetector.php
+++ src/Convert/Helpers/JpegQualityDetector.php
@@ -13,7 +13,13 @@ class JpegQualityDetector
 {
 
     /**
-     * Try to detect quality of jpeg using imagick extension
+     * Try to detect quality of jpeg using imagick extension.
+     *
+     * Note that the detection might fail for two different reasons:
+     * 1) Imagick is not installed
+     * 2) Imagick for some reason fails to detect quality for some images
+     *
+     * In both cases, null is returned.
      *
      * @param  string  $filename  A complete file path to file to be examined
      * @return int|null  Quality, or null if it was not possible to detect quality
@@ -26,7 +32,14 @@ class JpegQualityDetector
 
                 // The required function is available as from PECL imagick v2.2.2
                 if (method_exists($img, 'getImageCompressionQuality')) {
-                    return $img->getImageCompressionQuality();
+                    $quality = $img->getImageCompressionQuality();
+                    if ($quality === 0) {
+                        // We have experienced that this Imagick method returns 0 for some images,
+                        // (even though the imagemagick binary is able to detect the quality)
+                        // ie "/test/images/quality-undetectable-with-imagick.jpg". See #208
+                        $quality = null;
+                    }
+                    return $quality;
                 }
             } catch (\Exception $e) {
                 // Well well, it just didn't work out.
@@ -38,7 +51,17 @@ class JpegQualityDetector
 
 
     /**
-     * Try to detect quality of jpeg using imagick binary
+     * Try to detect quality of jpeg using imagick binary.
+     *
+     * Note that the detection might fail for three different reasons:
+     * 1) exec function is not available
+     * 2) the 'identify' command is not available on the system
+     * 3) imagemagick for some reason fails to detect quality for some images
+     *
+     * In the first two cases, null is returned.
+     * In the third case, 92 is returned. This is what imagemagick returns when it cannot detect the quality.
+     *    and unfortunately we cannot distinguish between the situation where the quality is undetectable
+     *    and the situation where the quality is actually 92 (at least, I have not found a way to do so)
      *
      * @param  string  $filename  A complete file path to file to be examined
      * @return int|null  Quality, or null if it was not possible to detect quality
@@ -58,7 +81,12 @@ class JpegQualityDetector
 
 
     /**
-     * Try to detect quality of jpeg using gmagick binary
+     * Try to detect quality of jpeg using graphicsmagick binary.
+     *
+     * It seems that graphicsmagick is never able to detect the quality! - and always returns
+     * the default quality, which is 75.
+     * However, as this might be solved in future versions, the method might be useful one day.
+     * But we treat "75" as a failure to detect and shall return null in that case.
      *
      * @param  string  $filename  A complete file path to file to be examined
      * @return int|null  Quality, or null if it was not possible to detect quality
@@ -69,7 +97,15 @@ class JpegQualityDetector
             // Try GraphicsMagick
             exec("gm identify -format '%Q' " . escapeshellarg($filename) . " 2>&1", $output, $returnCode);
             if ((intval($returnCode) == 0) && (is_array($output)) && (count($output) == 1)) {
-                return intval($output[0]);
+                $quality = intval($output[0]);
+
+                // It seems that graphicsmagick is (currently) never able to detect the quality!
+                // - and always returns 75 as a fallback
+                // We shall therefore treat 75 as a failure to detect. (#209)
+                if ($quality == 75) {
+                    return null;
+                }
+                return $quality;
             }
         }
         return null;
