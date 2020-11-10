35b54b246a8828d5322d289fe8277b8e3aa699d4
rosell-dk <it@rosell.dk>
2020-11-10 12:00:24 +0100

    Updated log message for exit code 127. It need not be missing file, it can also be missing library dependency. Closes #230

--- src/Convert/Converters/Cwebp.php
+++ src/Convert/Converters/Cwebp.php
@@ -433,7 +433,7 @@ class Cwebp extends AbstractConverter
         } else {
             $this->log('. Result: ');
             if ($returnCode == 127) {
-                $this->logLn('*Exec failed* (the cwebp binary was not found at path: ' . $binary . ')');
+                $this->logLn('*Exec failed* (the cwebp binary was not found at path: ' . $binary . ', or it had missing library dependencies)');
             } else {
                 if ($returnCode == 126) {
                     $this->logLn(
