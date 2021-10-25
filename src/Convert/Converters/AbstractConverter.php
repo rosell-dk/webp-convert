841d757b0c39433b3568cdd0d5f658c31f7b596f
rosell-dk <bjorn@rosell.dk>
2021-10-25 10:29:22 +0200

    Updated version to 2.7.0

--- src/Convert/Converters/AbstractConverter.php
+++ src/Convert/Converters/AbstractConverter.php
@@ -120,7 +120,7 @@ abstract class AbstractConverter
         $this->setProvidedOptions($options);
 
         if (!isset($this->options['_skip_input_check'])) {
-            $this->log('WebP Convert 2.6.0', 'italic');
+            $this->log('WebP Convert 2.7.0', 'italic');
             $this->logLn(' ignited.');
             $this->logLn('- PHP version: ' . phpversion());
             if (isset($_SERVER['SERVER_SOFTWARE'])) {
